b546c7eeff32e54ecb32e66569f8a5e589254659
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-07 01:31:42 +0200

    use routes and use expres.static to serve default profile image

--- src/server.js
+++ src/server.js
@@ -2,21 +2,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import path from 'path';
 import db from './db';
+import routes from './Routes';
 
 if (!process.env.PORT) {
   require('dotenv').config();
 }
 
-const port = process.env.PORT || 3001;
-
-
+const port = process.env.PORT || 1337;
 const app = express();
 
+const dir = path.join(__dirname, 'public/images');
+
+app.use(express.static(dir));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('hello world'));
-
 app.listen(port, () => console.log(`express is now listening to port ${port}`));
+
+routes(app);

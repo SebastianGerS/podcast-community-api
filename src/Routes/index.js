3919cc86a1a0454c27e953d236bc0c66e97de281
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-10 19:16:57 +0200

    add route for /search that uses SearchController

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,9 +1,10 @@
 
-import UserController from '../Controllers/UserController';
+import C from '../Controllers';
 
 export default (app) => {
   app.get('/', (req, res) => res.send('hello world'));
-  app.post('/users', UserController.create);
-  app.get('/users/:userId', UserController.find);
-  app.post('/login', UserController.auth);
+  app.post('/users', C.UserController.create);
+  app.get('/users/:userId', C.UserController.find);
+  app.post('/login', C.UserController.auth);
+  app.get('/search', C.SearchController.find);
 };

66178ae282a87eb8e7ebc360f5930350bf6e20a7
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-21 22:43:03 +0200

    add event controller to index

--- src/Controllers/index.js
+++ src/Controllers/index.js
@@ -1,10 +1,12 @@
 import UserController from './UserController';
 import SearchController from './SearchController';
 import AudioController from './AudioController';
+import EventController from './EventController';
+
 
 export default {
   UserController,
   SearchController,
   AudioController,
-
+  EventController,
 };

e709bc583219680265a22a99a35ddeb54b2f9bd4
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-21 22:43:49 +0200

    Add routes and middleware
    	* add route for getting logedin user (uses varifytoken middleware)
    	* add route for creating events (uses varifytoken middleware)
    	* chache update user methode to put and add use of varifytoken middleware

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,12 +1,15 @@
 
 import C from '../Controllers';
+import verifyToken from '../Middleware/verifyToken';
 
 export default (app) => {
   app.get('/', (req, res) => res.send('hello world'));
+  app.get('/me', verifyToken, C.UserController.me);
   app.post('/users', C.UserController.create);
   app.get('/users/:userId', C.UserController.find);
-  app.post('/users/:userId', C.UserController.update);
+  app.put('/users', verifyToken, C.UserController.update);
   app.post('/login', C.UserController.auth);
   app.get('/search', C.SearchController.find);
   app.get('/audio/:audioUrl', C.AudioController.stream);
+  app.post('/events', verifyToken, C.EventController.create);
 };

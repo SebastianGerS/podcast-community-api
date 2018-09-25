3852be4a2806afa3c37c7a0f3e55426e61ef6ecd
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-25 12:10:28 +0200

    abstract listen notes call and create to functions to search and find one specific podcast

--- src/Helpers/fetch.js
+++ src/Helpers/fetch.js
@@ -5,20 +5,32 @@ if (!process.env.JWT_SECRET) {
   require('dotenv').config();
 }
 
-
-export default async function fetchFromListenNotes(query) {
+export async function fetchFromListenNotes(path, query) {
   const headers = {
     'X-Mashape-Host': process.env.X_MASHAPE_HOST,
     'X-Mashape-key': process.env.X_MASHAPE_KEY,
   };
-  const { term, type, offset } = query;
 
   const response = await new Promise((resolve, reject) => {
-    Axios.get(`${process.env.X_MASHAPE_BASE_URL}/search?q=${term}&type=${type}&offset=${offset}`, { method: 'GET', headers })
+    Axios.get(`${process.env.X_MASHAPE_BASE_URL}/${path}/${query}`, { method: 'GET', headers })
       .then((data) => {
         resolve(data.data);
       })
       .then(data => resolve(data)).catch(error => reject(error));
   });
+  return response;
+}
+
+export async function searchListenNotes(query) {
+  const { term, type, offset } = query;
+
+  const response = await fetchFromListenNotes('/search', `?q=${term}&type=${type}&offset=${offset}`);
+
+  return response;
+}
+
+export async function fetchPodcastListenNotes(podcastId) {
+  const response = await fetchFromListenNotes('/podcasts/', podcastId);
+
   return response;
 }

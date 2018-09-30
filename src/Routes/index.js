b61f9cd34b8870f5d626ce239c232083318137b4
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-30 18:57:01 +0200

    Add admin routes for handeling users
    	* add routes to perform crud on users for admins
    	* add route for users to be able to delete ther own accounts

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -6,8 +6,13 @@ export default (app) => {
   app.get('/', (req, res) => res.send('hello world'));
   app.get('/me', verifyToken, C.UserController.me);
   app.post('/users', C.UserController.create);
+  app.post('/admin/users', verifyToken, C.UserController.create);
+  app.get('/users', verifyToken, C.UserController.findAll);
   app.get('/users/:userId', C.UserController.find);
   app.put('/users', verifyToken, C.UserController.update);
+  app.put('/admin/users/:userId', verifyToken, C.UserController.update);
+  app.delete('/users', verifyToken, C.UserController.delete);
+  app.delete('/admin/users/:userId', verifyToken, C.UserController.delete);
   app.post('/login', C.UserController.auth);
   app.get('/search', C.SearchController.find);
   app.get('/audio/:audioUrl', C.AudioController.stream);

33c683c15dcc797463f97ced407182e6d9b13b13
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-04-09 09:53:08 +0200

    Add avrageRating field to episode and podcast models
    Issue #12

--- src/Models/Episode.js
+++ src/Models/Episode.js
@@ -4,9 +4,10 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const EpisodeSchema = new Schema({
-  _id: [{ type: String, unique: true, required: true }],
-  podcast: [{ type: String, ref: 'Podcast' }],
+  _id: { type: String, unique: true, required: true },
+  podcast: { type: String, ref: 'Podcast' },
   ratings: [{ type: Schema.Types.ObjectId, ref: 'Rating ' }],
+  avrageRating: { type: Number, default: 0 },
 });
 
 mongoose.model('Episode', EpisodeSchema);
--- src/Models/Podcast.js
+++ src/Models/Podcast.js
@@ -4,8 +4,9 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const PodcastSchema = new Schema({
-  _id: [{ type: String, unique: true, required: true }],
+  _id: { type: String, unique: true, required: true },
   episodes: [{ type: String, ref: 'Episode' }],
+  avrageRating: { type: Number, default: 0 },
 });
 
 mongoose.model('Podcast', PodcastSchema);

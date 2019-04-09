8a76fd4acd06cfbc0b426ced00e9af4e2fc9fcbc
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-04-09 16:25:36 +0200

    Create findPodcasts method
    Issue #12

--- src/lib/Podcast.js
+++ src/lib/Podcast.js
@@ -1,8 +1,9 @@
 import R from 'ramda';
 import Podcast from '../Models/Podcast';
 import {
-  findOrCreate, handleUpdate,
+  find, findOrCreate, handleUpdate,
 } from '../Helpers/db';
 
 export const findOrCreatePodcast = R.partial(findOrCreate, [Podcast]);
 export const handlePodcastUpdate = R.partial(handleUpdate, [Podcast, ['episodes']]);
+export const findPodcasts = R.partial(find, [Podcast, { _id: 1, avrageRating: 1, episodes: 1 }]);

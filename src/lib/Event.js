173d17a80e783262464574e123e045e7e8680cd7
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-04-11 21:50:04 +0200

    Add new helper methodes to the event lib file
    * Add function to extract id from agent, objects and targets
    * Add function to format agent and tagets previusly populated with user data
    * Add correct key for kind in formatPopulatedEvent function
    
    Issue #8

--- src/lib/Event.js
+++ src/lib/Event.js
@@ -18,25 +18,70 @@ export function formatPopulatedEvent(event, object = event.object) {
       _id: event.agent.item._id,
       name: event.agent.item.username,
       image: event.agent.item.profile_img.thumb,
-      kind: event.agent.item.kind,
+      kind: event.agent.kind,
     },
     target: {
       _id: event.target.item._id,
       name: event.target.item.username,
       image: event.target.item.profile_img.thumb,
-      kind: event.target.item.kind,
+      kind: event.target.kind,
     },
     object,
     date: event.date,
   };
 }
 
+export function formatPopulatedUser(user) {
+  return {
+    _id: user.item._id,
+    name: user.item.username,
+    image: user.item.profile_img.thumb,
+    kind: user.kind,
+  };
+}
+
 export function extendObjectWithListenNotesItem(object, listenNotesItem) {
   return {
-    _id: object.item,
+    _id: object.item._id ? object.item._id : object.item,
     kind: object.kind,
     image: listenNotesItem.image,
     name: listenNotesItem.title,
-    parent_name: listenNotesItem.podcast ? listenNotesItem.podcast.title : undefined,
+    parent_name: listenNotesItem.podcast
+      ? listenNotesItem.podcast.title
+      : listenNotesItem.podcast_title,
   };
+}
+
+export function extractItemIds(eventsWithItems, itemType) {
+  const itemIds = [];
+
+  eventsWithItems.map((event) => {
+    if (event.object) {
+      if (event.object.item && !itemIds.includes(event.object.item)) {
+        itemIds.push(event.object.item);
+      }
+    }
+
+    if (event.target.kind === itemType && !itemIds.includes(event.target.item._id)) {
+      itemIds.push(event.target.item._id);
+    }
+
+    if (event.agent.kind === itemType && !itemIds.includes(event.agent.item._id)) {
+      itemIds.push(event.agent.item._id);
+    }
+
+    return event;
+  });
+
+  const stringOfItemIds = itemIds.reduce((ids, id, index) => {
+    if (index === 0 && itemIds.length > 1) {
+      return `${id},`;
+    } if (index === itemIds.length - 1) {
+      return `${ids},${id}`;
+    }
+
+    return `${ids},${id},`;
+  });
+
+  return stringOfItemIds;
 }

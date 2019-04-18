7cb06b65c6355d72f82c2c0a1b80d3e550d8aad5
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-04-18 21:38:04 +0200

    Limit call to fetchAndEmitToSubscribers
    Add func to limit call to fetchAndEmitToSubscribers triggered by new user subscribing to only be triggered if no previous subscribers exist
    
    This helps to reduce the number of calls to external api

--- src/lib/Subscriptions.js
+++ src/lib/Subscriptions.js
@@ -1,10 +1,12 @@
 import R from 'ramda';
 import Subscription from '../Models/Subscription';
 import {
-  findOrCreate, deleteOne, find, update,
+  findOrCreate, deleteOne, find, update, findById, create,
 } from '../Helpers/db';
 
 export const findOrCreateSubscription = R.partial(findOrCreate, [Subscription]);
 export const deleteSubscription = R.partial(deleteOne, [Subscription]);
 export const findSubscriptions = R.partial(find, [Subscription, { _id: 1, updated_at: 1 }]);
 export const updateSubscription = R.partial(update, [Subscription]);
+export const findSubscriptionById = R.partial(findById, [Subscription]);
+export const createSubscription = R.partial(create, [Subscription]);
